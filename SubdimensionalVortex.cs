2f0ad82a808b5aa2fd5b1ec6436d94db000359b3
Gearlink0 <gearlinkgmaster@aol.com>
2023-11-16 18:40:00 -0700

    Updated SubdimensionalVortex to be its own part instead of inheriting SpaceTimeVortex

--- SubdimensionalVortex.cs
+++ SubdimensionalVortex.cs
@@ -4,31 +4,61 @@ using System;
 using System.Collections.Generic;
 using XRL.Core;
 using XRL.Rules;
+using XRL.UI;
 
 namespace XRL.World.Parts
 {
   [Serializable]
-  public class LABYRINTHINETRAIL_SubdimensionalVortex : SpaceTimeVortex
+  public class LABYRINTHINETRAIL_SubdimensionalVortex : IPart
   {
-		public Cell targetCell;
-
+    public string DestinationZoneID;
+    public String destinationZoneIDState = "LABYRINTHINETRAIL_VortexDest_ZoneID";
+    public String repelledByTag = "LABYRINTHINETRAIL_RepelsSubdimensionalVortices";
+    public Cell targetCell;
+    // Choose a destination zone in JoppaWorld instead of the current world
     public String targetWorld = "JoppaWorld";
 
-    public String destinationZoneIDState = "LABYRINTHINETRAIL_VortexDest_ZoneID";
+    [NonSerialized]
+    private List<GameObject> Queue;
+    [NonSerialized]
+    private Dictionary<GameObject, Location2D> Locations;
 
-    public String repelledByTag = "LABYRINTHINETRAIL_RepelsSubdimensionalVortices";
+    public override bool AllowStaticRegistration() => true;
 
-		public override bool SpaceTimeAnomalyStationary() => true;
-
-		public override bool HandleEvent(EndTurnEvent E)
+    public override bool FireEvent(Event E)
     {
-      this.SpaceTimeAnomalyPeriodicEvents();
+      if (E.ID == "DefendMeleeHit")
+        this.ApplyVortex(E.GetGameObjectParameter("Attacker"));
+      return base.FireEvent(E);
+    }
+
+    public override bool WantEvent(int ID, int cascade)
+    {
+      return base.WantEvent(ID, cascade)
+      || ID == BeingConsumedEvent.ID
+      || ID == BlocksRadarEvent.ID
+      || ID == CanBeInvoluntarilyMovedEvent.ID
+      || ID == EndTurnEvent.ID
+      || ID == EnteredCellEvent.ID
+      || ID == GetMaximumLiquidExposureEvent.ID
+      || ID == InterruptAutowalkEvent.ID
+      || ID == ObjectEnteredCellEvent.ID
+      || ID == RealityStabilizeEvent.ID;
+    }
+
+    public override bool HandleEvent(BeingConsumedEvent E)
+    {
+      this.ApplyVortex(E.Actor);
       return base.HandleEvent(E);
     }
 
-    public void SpaceTimeAnomalyPeriodicEvents()
-		{
-			// Move the vortex
+    public override bool HandleEvent(BlocksRadarEvent E) => false;
+
+    public override bool HandleEvent(CanBeInvoluntarilyMovedEvent E) => E.Object != this.ParentObject && base.HandleEvent(E);
+
+		public override bool HandleEvent(EndTurnEvent E)
+    {
+      // Move the vortex
 			if ( this.targetCell != null )
 				this.ParentObject.SystemMoveTo( this.targetCell );
 
@@ -37,38 +67,106 @@ namespace XRL.World.Parts
       // repel them
       Predicate<Cell> pred = cell => cell.GetFirstObjectWithTag( repelledByTag ) == null;
       this.targetCell = currentCell.GetRandomLocalAdjacentCell(pred);
-		}
-
-    public override bool FireEvent(Event E)
-    {
-      if (E.ID == "DefendMeleeHit")
-        this.RequireDestinationZone();
-      return base.FireEvent(E);
-    }
-
-    public override bool HandleEvent(BeingConsumedEvent E)
-    {
-      this.RequireDestinationZone();
       return base.HandleEvent(E);
     }
 
     public override bool HandleEvent(EnteredCellEvent E)
     {
-      this.RequireDestinationZone();
+      if (!E.Cell.IsGraveyard())
+      {
+        foreach (GameObject GO in E.Cell.GetObjectsWithPartReadonly("Render"))
+        {
+          if (
+            this.ParentObject.IsValid()
+            && !this.ParentObject.IsInGraveyard()
+            && this.ParentObject.CurrentCell == E.Cell
+          )
+            this.ApplyVortex(GO);
+        }
+      }
       return base.HandleEvent(E);
     }
 
+    public override bool HandleEvent(GetMaximumLiquidExposureEvent E)
+    {
+      E.PercentageReduction = 100;
+      return false;
+    }
+
+    public override bool HandleEvent(InterruptAutowalkEvent E) => false;
+
     public override bool HandleEvent(ObjectEnteredCellEvent E)
     {
-      this.RequireDestinationZone();
+      if (!E.Cell.IsGraveyard())
+        this.ApplyVortex(E.Object);
       return base.HandleEvent(E);
+    }
+
+    public override bool HandleEvent(RealityStabilizeEvent E)
+    {
+      if (!E.Check(true))
+        return base.HandleEvent(E);
+      this.ParentObject.ParticleBlip("&m-");
+      this.DidX("collapse", "under the pressure of normality", ColorAsBadFor: this.ParentObject);
+      this.ParentObject.Destroy();
+      return false;
+    }
+
+    public bool ApplyVortex( GameObject GO )
+    {
+      if (this.ParentObject == GO || !SpaceTimeVortex.IsValidTarget(GO))
+        return false;
+
+      if (this.DestinationZoneID == null && !SpaceTimeVortex.ObjectCallsForExplicitTracking(GO))
+      {
+        IComponent<GameObject>.XDidYToZ(
+          GO,
+          "are",
+          "sucked into",
+          this.ParentObject,
+          EndMark: "!",
+          ColorAsBadFor: GO,
+          IndefiniteObject: true,
+          DescribeSubjectDirection: true
+        );
+        Location2D location = GO.CurrentCell?.location;
+        if (location != (Location2D) null)
+        {
+          if (this.Queue == null)
+            this.Queue = new List<GameObject>();
+          if (this.Locations == null)
+            this.Locations = new Dictionary<GameObject, Location2D>();
+          this.Queue.Add(GO);
+          this.Locations[GO] = location;
+          GO.RemoveFromContext();
+          GO.MakeInactive();
+        }
+        else
+          GO.Obliterate();
+      }
+      else
+      {
+        this.RequireDestinationZone();
+        Cell destinationCellFor = SpaceTimeVortex.GetDestinationCellFor(
+          this.DestinationZoneID,
+          GO,
+          this.ParentObject.CurrentCell
+        );
+        if (destinationCellFor == null || !GO.FireEvent(Event.New("SpaceTimeVortexContact", "Object", (object) this.ParentObject, "DestinationCell", (object) destinationCellFor)))
+          return false;
+        if (GO.IsPlayerLed() && !GO.IsTrifling)
+          Popup.Show("Your companion, " + GO.GetDisplayName(Short: true, WithIndefiniteArticle: true) + "," + GO.GetVerb("have") + " been sucked into " + this.ParentObject.t() + " " + The.Player.DescribeDirectionToward(this.ParentObject) + "!");
+        else
+          IComponent<GameObject>.XDidYToZ(GO, "are", "sucked into", this.ParentObject, EndMark: "!", ColorAsBadFor: GO, IndefiniteObject: true, DescribeSubjectDirection: true);
+        SpaceTimeVortex.Teleport(GO, destinationCellFor, this.ParentObject);
+      }
+      return true;
     }
 
     public void RequireDestinationZone()
     {
-      // Choose a destination zone in JoppaWorld isntead of the current world
-      if (!this.DestinationZoneID.IsNullOrEmpty())
-        return;
+      // if (!this.DestinationZoneID.IsNullOrEmpty())
+      //   return;
       if (!this.destinationZoneIDState.IsNullOrEmpty())
       {
         string zoneIDFromState = XRLCore.Core.Game.GetStringGameState( this.destinationZoneIDState );
@@ -83,6 +181,47 @@ namespace XRL.World.Parts
       }
       else
         this.DestinationZoneID = SpaceTimeVortex.GetRandomDestinationZoneID( this.targetWorld );
+
+      if (this.Queue == null || this.Locations == null)
+        return;
+
+      if (!this.DestinationZoneID.IsNullOrEmpty())
+      {
+        Zone targetZone = The.ZoneManager.GetZone( this.DestinationZoneID );
+        foreach (GameObject gameObject in this.Queue)
+        {
+          try
+          {
+            if (GameObject.Validate(gameObject) && !gameObject.HasContext())
+            {
+              Location2D location = this.Locations[gameObject];
+              Cell destinationCellFor = SpaceTimeVortex.GetDestinationCellFor(this.DestinationZoneID, gameObject, location);
+              if (destinationCellFor != null)
+              {
+
+                SpaceTimeVortex.Teleport(gameObject, destinationCellFor, this.ParentObject, targetZone?.GetCell(location), false);
+                gameObject.MakeActive();
+              }
+              else
+                gameObject.Obliterate();
+            }
+          }
+          catch (Exception ex)
+          {
+            MetricsManager.LogException("Space-time vortex queue unload", ex);
+          }
+        }
+      }
+      else
+      {
+        foreach (GameObject Object in this.Queue)
+        {
+          if (GameObject.Validate(Object) && !Object.HasContext())
+            Object.Obliterate();
+        }
+      }
+      this.Queue = (List<GameObject>) null;
+      this.Locations = (Dictionary<GameObject, Location2D>) null;
     }
 
     public override bool Render(RenderEvent E)
